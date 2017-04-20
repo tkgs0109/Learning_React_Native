9becd3cf16bbd464451853d74c44cd5bd937eb9c
tkgs0109 <subtkgs@gmail.com>
2017-04-20 22:53:54 +0900

    [WIP]WeatherProject

--- WeatherProject/Forecast.js
+++ WeatherProject/Forecast.js
@@ -1,43 +1,45 @@
-var React = require('react-native');
-var {
+import React, {
+  Component
+} from 'react';
+
+import {
   StyleSheet,
   Text,
   View
-} = React;
+} from 'react-native';
 
-var Forecast = React.createClass({
-  render: function() {
+class Forecast extends Component{
+  render() {
     return (
       <View>
-      <Text style={styles.bigText}>
+      <Text style="{styles.bigText}">
       {this.props.main}
       </Text>
-      <Text style={styles.mainText}>
+      <Text style="{styles.mainText}">
       Current conditions: {this.props.description}
       </Text>
-      <Text style={styles.bigText}>
+      <Text style="{styles.bigText}">
       {this.props.temp}°F
       </Text>
       </View>
     );
   }
+}
+
+const styles = StyleSheet.create({
+  bigText: {
+    flex: 2,
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  mainText: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
 });
 
-var styles = StyleSheet.create({
-  bigText: {
-  flex: 2,
-  fontSize: 20,
-  textAlign: 'center',
-  margin: 10,
-  color: '#FFFFFF'
-},
-
-mainText: {
-  flex: 1,
-  fontSize: 16,
-  textAlign: 'center',
-  color: '#FFFFFF'
-}
-})
-
-module.exports = Forecast;
+export default Forecast;
--- WeatherProject/index.android.js
+++ WeatherProject/index.android.js
@@ -1,12 +1,5 @@
-import React, {
-  Component
-} from 'react';
-
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
 import WeatherProject from './WeatherProject';
